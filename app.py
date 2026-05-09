f88dc431dff04473bad666c4687379ce3b6a071d
indira <indirasgowda92@gmail.com>
2026-05-09 16:44:35 +0530

    Ready for deployment

--- app.py
+++ app.py
@@ -21,3 +21,5 @@ def home():
 if __name__ == "__main__":
     print("Starting Flask AI Service...")
     app.run(debug=True, host="127.0.0.1", port=5000)
+    if __name__ == "__main__":
+        app.run(host="0.0.0.0", port=10000)
